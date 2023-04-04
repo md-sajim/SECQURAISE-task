069af7885908079c162858ec943fc7164e0dadb0
sajim <mdabutaliveb@gmail.com>
2023-04-05 03:04:15 +0600

    user detected

--- src/componant/Home/Home.js
+++ src/componant/Home/Home.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css'
-import { FaBars } from 'react-icons/fa';
+import { FaBars, FaSlidersH } from 'react-icons/fa';
 
 const Home = () => {
+    const [detecteduser, setDectedUser] = useState({})
+    const [userdata, setUserData] = useState([])
+    useEffect(() => {
+        fetch('fackData.json')
+            .then(res => res.json())
+            .then(data => setUserData(data))
+    }, [])
+    console.log(detecteduser)
     return (
         <div className='home-container'>
             <div className='laft-bar'>
@@ -10,12 +18,34 @@ const Home = () => {
                     <FaBars />
                 </button>
             </div>
-            <div>
+            <div className='details-container'>
                 <div>
                     <h1>tast-1</h1>
                 </div>
                 <div>
-                    tast-2
+                    <div className='evet-header'>
+                        <h2>Event</h2>
+                        <button><FaSlidersH /></button>
+                    </div>
+                    <dir className='enter-user'>
+                        {
+                            userdata.map((entry, id) => <div
+
+                                key={id}
+                                className='parsone-container'
+                                onClick={() => { setDectedUser(entry) }}
+
+
+                            >
+                                <div>
+                                    <h3 style={{ fontWeight: "600", fontSize: "24px" }}>{entry.ID}: {entry.Location}</h3>
+                                    <p><small>{entry.Date} {entry.Time}</small></p>
+                                </div>
+                                <p style={{ fontWeight: "600", fontSize: "20px" }}>Person detected.</p>
+                            </div>
+                            )
+                        }
+                    </dir>
                 </div>
             </div>
         </div>
